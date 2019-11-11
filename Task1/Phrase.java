92a71fd2c845e568568bd4e7de2b68a0e6176a1a
SimpleJavaUser <50578518+SimpleJavaUser@users.noreply.github.com>
2019-11-11 13:29:24 +0300

    Update Phrase.java

--- Task1/Phrase.java
+++ Task1/Phrase.java
@@ -1,7 +1,9 @@
 package by.epam.les10.main;
 
 public class Phrase {
-	    private String value="";
+	    private String value="";// String List<Word> value
+	// это декомпозиция задачи - мредложение состоит из списка слов (упрощая)
+	// тоже и в тексте
 
 	    public String getValue() {
 	        return value;
