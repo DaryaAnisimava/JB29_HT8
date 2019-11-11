371e1553cf41758ebc8e559e8650567b598137fa
SimpleJavaUser <50578518+SimpleJavaUser@users.noreply.github.com>
2019-11-11 13:30:30 +0300

    Update Tulip.java

--- Task3/Tulip.java
+++ Task3/Tulip.java
@@ -1,7 +1,8 @@
 package by.epam.les10.main;
 
 public class Tulip extends Flower{
-    private final static String name = "Тюльпан";
+    private final static String name = "Тюльпан";// нужно более подробно прочувствовать тему нАследование
+    // это не корректное наследование, т.к. оно не привносит в производный класс ничего, что нельзы сделать через базовый
     public Tulip(double price, Color color) {
         super(price, name, color);
     }
